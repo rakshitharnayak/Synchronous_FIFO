56c185b19aed79f3218f43004a468088354c0411
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-07 10:19:38 +0530

    Create defines.sv

---
+++ defines.sv
@@ -0,0 +1,8 @@
+
+//////////////////////////////////////////////////////////////////////////
+ // `define no_of_trans 20
+`define DATA_W 128           // Data width
+`define DEPTH 1024          // Depth of FIFO
+`define UPP_TH 4           // Upper threshold to generate Almost-full
+`define LOW_TH 2           //  Lower threshold to generate Almost-empty
+//////////////////////////////////////////////////////////////////////////
