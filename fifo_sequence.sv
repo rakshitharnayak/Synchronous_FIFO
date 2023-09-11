b2cd9a57e3ed60d4276d4af1f7ca9aede2c58616
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-11 16:28:14 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,105 +1,41 @@
-class fifo_sequence1 extends uvm_sequence#(fifo_seq_item);
-  `uvm_object_utils(fifo_sequence1)
-  fifo_seq_item tr;
-  function new(string name = "fifo_sequence1");
+class fifo_sequence extends uvm_sequence#(fifo_seq_item);
+	`uvm_object_utils(fifo_sequence)
+  fifo_seq_item req;
+  function new(string name = "fifo_sequence");
     super.new(name);
   endfunction
 
   //// FIFO test 1
-// write FIFO from empty to full -> read FIFO from full to empty
+// write FIFO from empty to full 
     task body();
-      for (int i = 0; i < DEPTH *2; i++) begin 
-            tr = data_transaction::type_id::create("tr");
-            start_item(tr);
-            
-        if (i < DEPTH) begin
-          if (!tr.randomize() with {tr.i_wren == 1'b1; tr.rden == 1'b0;}) begin
-            `uvm_error("Sequence", "Randomization failure for transaction")
-                end 
-            end
-            else begin
-                if (!tr.randomize() with {tr.i_wren == 1'b0; tr.rden == 1'b1;}) begin
-                  `uvm_error("Sequence", "Randomization failure for transaction")
-                end 
-            end
-
-            finish_item(tr); 
-        end
-    endtask
-endclass: fifo_sequence1
-
-
-//FOFO test 2
-// Write FIFO from empty to almost full, and then read/write at the same time
-class fifo_sequence2 extends uvm_sequence #(fifo_seq_item);
-  `uvm_object_utils(fifo_sequence2)
-  fifo_seq_item tr;
-  
-  function new(string name = "fifo_sequence2");
-    super.new(name);
-  endfunction
-	
-	task body();
-    for (int i = 0; i < DEPTH *2; i++) begin 
-          	tr = data_transaction::type_id::create("tr");
-            start_item(tr);
-      
-     //i<1020   -> write till almost full    
-            if (i < (DEPTH - UPP_TH)) begin
-                if (!tr.randomize() with {tr.i_wren == 1'b1; tr.rden == 1'b0;}) begin
-                    `uvm_error("Sequence", "Randomization failure for trasaction")
-                end 
-            end
-      
-      // then read and write at the same time
-            else begin    
-                if (!tr.randomize() with {tr.i_wren == 1'b1; tr.rden == 1'b1;}) begin
-                    `uvm_error("Sequence", "Randomization failure for trasaction")
-                end 
-            end
-
-            finish_item(tr); 
-        end
-	endtask
-endclass: fifo_sequence2
-
-
-//FIFO test3
-// Write FIFO from empty to almostfull, and then read till almost empty, and then read/write at the same time
-class fifo_sequence3 extends uvm_sequence #(fifo_seq_item);
-  `uvm_object_utils(fifo_sequence3)
-  fifo_seq_item tr;
-  
-  function new(string name = "fifo_sequence3");
-    super.new(name);
-  endfunction
-	
-	task body();
-    for (int i = 0; i < DEPTH *2; i++) begin 
-          	tr = data_transaction::type_id::create("tr");
-            start_item(tr);
-      
-     //i<1020   -> write till almost full    
-            if (i < (DEPTH - UPP_TH)) begin
-                if (!tr.randomize() with {tr.i_wren == 1'b1; tr.rden == 1'b0;}) begin
-                    `uvm_error("Sequence", "Randomization failure for trasaction")
-                end 
-            end
-      
-      // then read till almost full
-      else if(i>=1020) begin    
-        if (!tr.randomize() with {tr.i_wren == 1'b0; tr.rden == 1'b1;}) begin
-                    `uvm_error("Sequence", "Randomization failure for trasaction")
-                end 
-            end
-      //then read and write at the same time
-      else begin
-        if (!tr.randomize() with {tr.i_wren == 1'b1; tr.rden == 1'b1;}) begin
-                    `uvm_error("Sequence", "Randomization failure for trasaction")
-                end 
-            end
-
-            finish_item(tr); 
-        end
-	endtask
+	     end
+	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE REQs ********"), UVM_LOW)
+	    repeat(DEPTH) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+	 assert(req.randomize() with {i_wren == 1; i_rden = 0;});
+      finish_item(req);
+    end
+	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
+ repeat(DEPTH) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+	 assert(req.randomize() with {i_wren == 0; i_rden = 1;});
+      finish_item(req);
+    end
+	    `uvm_info(get_type_name(), $sformatf("******** Generate 20 WRITE AND READ IN PARALLEL REQs ********"), UVM_LOW)
+	    repeat(DEPTH) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+	    assert(req.randomize() with {i_wren == 1; i_rden = 1;});
+      finish_item(req);
+    end
+	    `uvm_info(get_type_name(), $sformatf("******** Generate 20 WRITE AND READ IN ALTERNATE REQs ********"), UVM_LOW)
+	    repeat(DEPTH) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+		    assert(req.randomize() with {i_wren == 1; i_rden = 0;});
+      finish_item(req);
+    end
+  endtask
 endclass: fifo_sequence3
