5bceed893a702fcf2e76a17f2b1363afbaac7573
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-12 12:29:02 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -14,31 +14,34 @@ class fifo_driver extends uvm_driver#(f_seq_item);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
-    vif.d_mp.d_cb.wr <= 'b0;
-    vif.d_mp.d_cb.rd <= 'b0;
-    vif.d_mp.d_cb.data_in <= 'b0;
+    if(vif.d_mp.d_cb.rstn==0)
+       @(posedge vif.d_mp)
+          begin
+    vif.d_mp.d_cb.i_wren <= 'b0;
+    vif.d_mp.d_cb.i_rden <= 'b0;
+    vif.d_mp.d_cb.i_wrdata <= 'b0;
     forever begin
-      seq_item_port.get_next_item(req);
-      if(req.wr == 1)
-        main_write(req.data_in);
-      else if(req.rd == 1)
+      seq_item_port.get_next_item(tr);
+      if(tr.i_wren == 1)
+        main_write(tr.i_wrdata);
+      else if(tr.i_rden == 1)
         main_read();
       seq_item_port.item_done();
     end
   endtask
   
-  virtual task main_write(input [7:0] din);
-    @(posedge vif.d_mp.clk)
-    vif.d_mp.d_cb.wr <= 'b1;
-    vif.d_mp.d_cb.data_in <= din;
-    @(posedge vif.d_mp.clk)
-    vif.d_mp.d_cb.wr <= 'b0;
+  virtual task main_write(input [DATA_W - 1:0] din);
+    @(posedge vif.d_mp)
+    vif.d_mp.d_cb.i_wren <= 'b1;
+    vif.d_mp.d_cb.i_wrdta <= din;
+     @(posedge vif.d_mp)
+     vif.d_mp.d_cb.i_wren <= 'b0;
   endtask
   
   virtual task main_read();
-    @(posedge vif.d_mp.clk)
+    @(posedge vif.d_mp)
     vif.d_mp.d_cb.rd <= 'b1;
-    @(posedge vif.d_mp.clk)
+    @(posedge vif.d_mp)
     vif.d_mp.d_cb.rd <= 'b0;
   endtask
 
