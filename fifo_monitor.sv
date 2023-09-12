48ddf2c1456269e11d3677c1df2eb26347a9eb41
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-12 14:43:40 +0530

    Update fifo_monitor.sv

--- fifo_monitor.sv
+++ fifo_monitor.sv
@@ -1,6 +1,6 @@
 class fifo_monitor extends uvm_monitor;
   virtual fifo_if vif;
-  fifo_seq_item item_got;
+  fifo_seq_item tr;
   uvm_analysis_port#(fifo_seq_item) item_got_port;
   `uvm_component_utils(fifo_monitor)
   
@@ -20,25 +20,53 @@ class fifo_monitor extends uvm_monitor;
   virtual task run_phase(uvm_phase phase);
     forever begin
       @(posedge vif.m_mp)
-      if(vif.m_mp.m_cb.i_wren == 1)begin
-        $display("\ni_wren -> write enable is high");
+      if(vif.m_mp.m_cb.i_wren == 1 && vif.m_mp.m_cb.i_rden == 1)begin
+        $display("\n write enable and read enable are high");
         tr.i_wrdata = vif.m_mp.m_cb.i_wrdata;
         tr.i_wren = 'b1;
-        tr.i_rden = 'b0;
+        tr.i_rden = 'b1;
         tr.o_full = vif.m_mp.m_cb.o_full;
         tr.o_alm_full = vif.m_mp.m_cb.o_alm_full;
-        item_got_port.write(tr);
-      end
-      else if(vif.m_mp.m_cb.i_rden == 1)begin
-        @(posedge vif.m_mp)
-        $display("\ni_rden -> read enable is high");
-        tr.o_rddata = vif.m_mp.m_cb.o_rddata;
-        tr.i_rden = 'b1;
-        tr.i_wren = 'b0;
         tr.o_empty = vif.m_mp.m_cb.o_empty;
         tr.o_alm_empty = vif.m_mp.m_cb.o_alm_empty;
         item_got_port.write(tr);
       end
+      else if(vif.m_mp.m_cb.i_wren == 1 && vif.m_mp.m_cb.i_rden == 0)begin
+        @(posedge vif.m_mp)
+        $display("\ni_rden -> read enable is high");
+        tr.o_rddata = vif.m_mp.m_cb.o_rddata;
+        tr.i_wren = 'b1;
+        tr.i_rden = 'b0;
+        tr.o_full = vif.m_mp.m_cb.o_full;
+        tr.o_alm_full = vif.m_mp.m_cb.o_alm_full;
+        tr.o_empty = vif.m_mp.m_cb.o_empty;
+        tr.o_alm_empty = vif.m_mp.m_cb.o_alm_empty;
+        item_got_port.write(tr);
+      end
+      else if(vif.m_mp.m_cb.i_wren == 0 && vif.m_mp.m_cb.i_rden == 1)begin
+        @(posedge vif.m_mp)
+        $display("\ni_rden -> read enable is high");
+        tr.o_rddata = vif.m_mp.m_cb.o_rddata;
+        tr.i_wren = 'b0;
+        tr.i_rden = 'b1;
+        tr.o_full = vif.m_mp.m_cb.o_full;
+        tr.o_alm_full = vif.m_mp.m_cb.o_alm_full;
+        tr.o_empty = vif.m_mp.m_cb.o_empty;
+        tr.o_alm_empty = vif.m_mp.m_cb.o_alm_empty;
+        item_got_port.write(tr);
+      end
+      else if(vif.m_mp.m_cb.i_wren == 0 && vif.m_mp.m_cb.i_rden == 0)begin
+        @(posedge vif.m_mp)
+        $display("\ni_rden -> read enable is high");
+        tr.o_rddata = vif.m_mp.m_cb.o_rddata;
+        tr.i_wren = 'b0;
+        tr.i_rden = 'b0;
+        tr.o_full = vif.m_mp.m_cb.o_full;
+        tr.o_alm_full = vif.m_mp.m_cb.o_alm_full;
+        tr.o_empty = vif.m_mp.m_cb.o_empty;
+        tr.o_alm_empty = vif.m_mp.m_cb.o_alm_empty;
+        item_got_port.write(tr);
+    end
     end
   endtask
 endclass
