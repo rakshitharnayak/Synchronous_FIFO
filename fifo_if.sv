dc7c4a69404693d41761f4830cb7d4c38dfca7aa
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-12 11:38:27 +0530

    Update fifo_if.sv

--- fifo_if.sv
+++ fifo_if.sv
@@ -1,7 +1,24 @@
-interface fifo_if;
-    logic clk, rstn, i_wren, i_rden, o_alm_empty, o_empty, o_alm_full, o_full;
+interface fifo_if(input clk, rstn);
+    logic i_wren, i_rden, o_alm_empty, o_empty, o_alm_full, o_full;
     logic [DATA_W - 1:0] i_wrdata;
     logic [DATA_W - 1:0] o_rddata;
+
+    clocking d_cb @(posedge clk);
+    default input #0 output #0;
+    output i_wren;
+    output i_rden;
+    output i_wrdata;  
+    input rstn;
+  endclocking
+
+    clocking m_cb @(posedge clk);
+    default input #0 output #0;  
+    input  o_alm_empty, o_empty, o_alm_full, o_full, o_rddata;    
+  endclocking
+
+  modport d_mp (clocking d_cb);
+  modport m_mp (clocking m_cb);
+      
 endinterface 
 
 
