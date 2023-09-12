826ae7179c22dfabd40c706ef442d5d534cf9d32
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-12 11:22:39 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -8,10 +8,9 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
   //// FIFO test 1
 // write FIFO from empty to full 
     task body();
-	     end
-	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE REQs ********"), UVM_LOW)
-	    repeat(DEPTH) begin
-      req = fifo_seq_item::type_id::create("req");
+	 `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE REQs ********"), UVM_LOW)
+	  repeat(DEPTH) begin
+      	  req = fifo_seq_item::type_id::create("req");
       start_item(req);
 	 assert(req.randomize() with {i_wren == 1; i_rden = 0;});
       finish_item(req);
@@ -30,7 +29,7 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
 	    assert(req.randomize() with {i_wren == 1; i_rden = 1;});
       finish_item(req);
     end
-	    `uvm_info(get_type_name(), $sformatf("******** Generate 20 WRITE AND READ IN ALTERNATE REQs ********"), UVM_LOW)
+	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN ALTERNATE REQs ********"), UVM_LOW)
 	    repeat(DEPTH) begin
       req = f_sequence_item::type_id::create("req");
       start_item(req);
@@ -41,4 +40,4 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
       finish_item(req);
     end
   endtask
-endclass: fifo_sequence3
+endclass
