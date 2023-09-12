bd88b324aaff278831b3b03e211034009fc92e35
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-12 15:24:49 +0530

    Update fifo_environment.sv

--- fifo_environment.sv
+++ fifo_environment.sv
@@ -1,1 +1,20 @@
-f
+class fifo_environment extends uvm_env;
+  fifo_agent agt;
+  fifo_scoreboard scb;
+  `uvm_component_utils(fifo_environment)
+  
+  function new(string name = "fifo_environment", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    agt = fifo_agent::type_id::create("agt", this);
+    scb = fifo_scoreboard::type_id::create("scb", this);
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    agt.mon.item_got_port.connect(scb.item_got_export);
+  endfunction
+  
+endclass
