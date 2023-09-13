d493bff8396426aa8323a37c6f2287f6a3d6cab6
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-13 12:00:37 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,3 +1,6 @@
+//SEQUENCE
+`include "defines.sv"
+
 class fifo_sequence extends uvm_sequence#(fifo_seq_item);
 	`uvm_object_utils(fifo_sequence)
   	fifo_seq_item req;
@@ -9,37 +12,37 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
     task body();
       
 	 `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE REQs ********"), UVM_LOW)
-	  repeat(DEPTH) begin
+      repeat(`DEPTH) begin
         req = fifo_seq_item::type_id::create("req");
       	start_item(req);
-        assert(req.randomize() with {i_wren == 1; i_rden = 0;});
+        assert(req.randomize() with {i_wren == 1; i_rden == 0;});
       	finish_item(req);
       end
       
 	 `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
- 	  repeat(DEPTH) begin
+      repeat(`DEPTH) begin
         req = fifo_seq_item::type_id::create("req");
         start_item(req);
-        assert(req.randomize() with {i_wren == 0; i_rden = 1;});
+        assert(req.randomize() with {i_wren == 0; i_rden == 1;});
         finish_item(req);
       end
       
 	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN PARALLEL REQs ********"), UVM_LOW)
-	    repeat(DEPTH) begin
-          req = f_sequence_item::type_id::create("req");
+      repeat(`DEPTH) begin
+          req = fifo_seq_item::type_id::create("req");
           start_item(req);
-          assert(req.randomize() with {i_wren == 1; i_rden = 1;});
+          assert(req.randomize() with {i_wren == 1; i_rden == 1;});
           finish_item(req);
         end
       
       `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN ALTERNATE REQs ********"), UVM_LOW)
-	    repeat(DEPTH) begin
-          req = f_sequence_item::type_id::create("req");
+      repeat(`DEPTH) begin
+          req = fifo_seq_item::type_id::create("req");
           start_item(req);
-          assert(req.randomize() with {i_wren == 1; i_rden = 0;});
+          assert(req.randomize() with {i_wren == 1; i_rden == 0;});
           finish_item(req);	  
           start_item(req);
-          assert(req.randomize() with {i_wren == 0; i_rden = 1;});
+          assert(req.randomize() with {i_wren == 0; i_rden == 1;});
           finish_item(req);
         end
       
