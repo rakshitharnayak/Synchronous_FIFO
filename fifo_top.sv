0b47d942cf46f4fccd1450d187b34f336cd180af
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-13 11:45:33 +0530

    Update fifo_top.sv

--- fifo_top.sv
+++ fifo_top.sv
@@ -1,7 +1,17 @@
-import uvm_pkg::*;
-import fifo_pkg::*;
+ //TOP
+   import uvm_pkg::*;
 `include "uvm_macros.svh"
+`include "defines.sv"
 `include "fifo_if.sv"
+// `include "fifo_test.sv"
+`include "fifo_seq_item.sv"
+`include "fifo_sequence.sv"
+`include "fifo_sequencer.sv"
+`include "fifo_driver.sv"
+`include "fifo_monitor.sv"
+`include "fifo_agent.sv"
+`include "fifo_scoreboard.sv"
+`include "fifo_environment.sv"
 `include "fifo_test.sv"
 
 module tb;
@@ -28,7 +38,7 @@ module tb;
                .o_alm_full(fif.o_alm_full),
                .o_alm_empty(fif.o_alm_empty),
                .o_empty(fif.o_empty),
-               .o_rddta(fif.o_rddata));
+               .o_rddata(fif.o_rddata));
   
   initial begin
     uvm_config_db#(virtual fifo_if)::set(null, "", "vif", fif);
