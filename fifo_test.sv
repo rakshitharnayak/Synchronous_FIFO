a06e1e9e49f4037fa97d8679768c59a109ef0c9b
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-13 11:46:35 +0530

    Update fifo_test.sv

--- fifo_test.sv
+++ fifo_test.sv
@@ -1,3 +1,4 @@
+ //TEST
 class fifo_test extends uvm_test;
   fifo_sequence seq;
   fifo_environment env;
@@ -9,15 +10,15 @@ class fifo_test extends uvm_test;
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    seq = f_sequence::type_id::create("seq", this);
-    env = f_environment::type_id::create("env", this);
+    seq = fifo_sequence::type_id::create("seq", this);
+    env = fifo_environment::type_id::create("env", this);
   endfunction
   
   virtual task run_phase(uvm_phase phase);
     phase.raise_objection(this);
     seq.start(env.agt.seqr);
     phase.drop_objection(this);
-    // phase.phase_done.set_drain_time(this, 100);
+    phase.phase_done.set_drain_time(this, 100);
   endtask
   
 endclass
