d7d32e178b8f03d086d50c9a5be7175e4f0c1d37
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-14 14:48:45 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -27,24 +27,25 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
         finish_item(req);
       end
       
-	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN PARALLEL REQs ********"), UVM_LOW)
-      repeat(`DEPTH) begin
-          req = fifo_seq_item::type_id::create("req");
-          start_item(req);
-          assert(req.randomize() with {i_wren == 1; i_rden == 1;});
-          finish_item(req);
-        end
+// 	    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN PARALLEL REQs ********"), UVM_LOW)
+//       repeat(`DEPTH) begin
+//           req = fifo_seq_item::type_id::create("req");
+//           start_item(req);
+//           assert(req.randomize() with {i_wren == 1; i_rden == 1;});
+//                 `uvm_info("Sequence",$sformatf("randomised value, i_wren:%0d , i_rden:%0d" ,req.i_wren,req.i_rden), UVM_NONE);
+//           finish_item(req);
+//         end
       
-      `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN ALTERNATE REQs ********"), UVM_LOW)
-      repeat(`DEPTH) begin
-          req = fifo_seq_item::type_id::create("req");
-          start_item(req);
-          assert(req.randomize() with {i_wren == 1; i_rden == 0;});
-          finish_item(req);	  
-          start_item(req);
-          assert(req.randomize() with {i_wren == 0; i_rden == 1;});
-          finish_item(req);
-        end
+//       `uvm_info(get_type_name(), $sformatf("******** Generate 1024 WRITE AND READ IN ALTERNATE REQs ********"), UVM_LOW)
+//       repeat(`DEPTH) begin
+//           req = fifo_seq_item::type_id::create("req");
+//           start_item(req);
+//           assert(req.randomize() with {i_wren == 1; i_rden == 0;});
+//           finish_item(req);	  
+//           start_item(req);
+//           assert(req.randomize() with {i_wren == 0; i_rden == 1;});
+//           finish_item(req);
+//         end
       
     endtask
 endclass
