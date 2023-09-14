c9bbc11c29567619345efa2ddedf14dfc056cf6b
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-14 14:46:32 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -1,4 +1,5 @@
 //DRIVER
+
 class fifo_driver extends uvm_driver#(fifo_seq_item);
   virtual fifo_if vif;
   fifo_seq_item tr;
@@ -10,41 +11,42 @@ class fifo_driver extends uvm_driver#(fifo_seq_item);
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual fifo_if)::get(this, "", "vif", vif))
+    if(!uvm_config_db#(virtual fifo_if)::get(this, "", "vif", vif))begin
       `uvm_fatal("Driver: ", "No vif is found!")
+    end
   endfunction
 
   virtual task run_phase(uvm_phase phase);
-    if(vif.d_mp.d_cb.rstn==0) begin
-       @(posedge vif.d_mp)
-      vif.d_mp.d_cb.i_wren <= 'b0;
-      vif.d_mp.d_cb.i_rden <= 'b0;
-      vif.d_mp.d_cb.i_wrdata <= 'b0;
-    end
-    else begin
-      forever begin
+    forever begin
+      if(vif.d_mp.d_cb.rstn==0) begin
+        @(posedge vif.d_mp.d_cb)
+        vif.d_mp.d_cb.i_wren <= 'b0;
+        vif.d_mp.d_cb.i_rden <= 'b0;
+        vif.d_mp.d_cb.i_wrdata <= 'b0;
+      end
         seq_item_port.get_next_item(tr);
-        if(tr.i_wren == 1)
+        if(tr.i_wren == 1)begin
           main_write(tr.i_wrdata);
-        else if(tr.i_rden == 1)
+        end
+        else if(tr.i_rden == 1)begin
           main_read();
+        end
         seq_item_port.item_done();
       end
-    end
   endtask
   
-    virtual task main_write(input [`DATA_W - 1:0] din);
-    @(posedge vif.d_mp)
-    vif.d_mp.d_cb.i_wren <= 'b1;
-    vif.d_mp.d_cb.i_wrdata <= din;
-     @(posedge vif.d_mp)
-     vif.d_mp.d_cb.i_wren <= 'b0;
+  virtual task main_write(input [`DATA_W - 1:0] din);
+      @(posedge vif.d_mp.d_cb)
+      vif.d_mp.d_cb.i_wren <= 'b1;
+      vif.d_mp.d_cb.i_wrdata <= din;
+      @(posedge vif.d_mp.d_cb)
+      vif.d_mp.d_cb.i_wren <= 'b0;
   endtask
   
   virtual task main_read();
-    @(posedge vif.d_mp)
+    @(posedge vif.d_mp.d_cb)
     vif.d_mp.d_cb.i_rden <= 'b1;
-    @(posedge vif.d_mp)
+    @(posedge vif.d_mp.d_cb)
     vif.d_mp.d_cb.i_rden <= 'b0;
   endtask
 
