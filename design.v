b369f653f2125b69ce6c81164128785080075695
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-09-18 09:54:32 +0530

    Update design.v

--- design.v
+++ design.v
@@ -1,4 +1,4 @@
-module SYN_FIFO #(parameter DEPTH=1024, DATA_W=128, UPP_TH= 4 , LOW_TH= 2 ) (
+module SYN_FIFO #(parameter DEPTH=16, DATA_W=128, UPP_TH= 4 , LOW_TH= 2 ) (
     input wire clk,         // Clock signal
     input wire rstn,       // Reset signal
     input wire i_wren,      // Write enable signal
@@ -19,20 +19,20 @@ module SYN_FIFO #(parameter DEPTH=1024, DATA_W=128, UPP_TH= 4 , LOW_TH= 2 ) (
   reg [9:0] write_ptr = 0;  // Write pointer
   reg [9:0] count = 0;      // Count of valid data in the FIFO
 
-  always @(posedge clk or posedge rstn ) begin
+  always @(posedge clk ) begin
     if (rstn == 0) begin
       read_ptr <= 0;
       write_ptr <= 0;
       count <= 0;
       o_full <= 0;
       o_alm_full <= 0;
-      o_alm_empty <= 1;
+      o_alm_empty <= 0;
       o_empty <= 1;  // FIFO is empty initially
     end else begin
       if (i_wren && (count < DEPTH)) begin
         fifo_mem[write_ptr] <= i_wrdata;
         write_ptr <= write_ptr + 1;
-        count <= count + 1;
+        count++;
         // FIFO is not empty when writing
       end
       if (i_rden && (count > 0)) begin
@@ -41,34 +41,51 @@ module SYN_FIFO #(parameter DEPTH=1024, DATA_W=128, UPP_TH= 4 , LOW_TH= 2 ) (
         count <= count - 1;
 
       end
-      if(count == 1023) begin
+      if(count == DEPTH) begin
         o_full <= 1; // Update full signal
-         o_empty <= 0;
+        o_empty <= 0;
+       o_alm_empty <= 0;
+        o_alm_full <=0;
+        
       end
    
         
       else if (count ==0) begin
          o_empty <= 1;// Update empty signal
         o_full <= 0;
+        o_alm_empty <= 0;
+        o_alm_full <=0;
    
       end
       
       else if( count >= (DEPTH -UPP_TH-1) && count <DEPTH)
         begin
           o_alm_full <=1;
+          o_alm_empty <= 0;
+		 o_empty <= 0;// Update empty signal
+        o_full <= 0;
         end
       
-            else if( 0<count <= LOW_TH )
+            else if( count > 0 && count <= LOW_TH)
         begin
           o_alm_empty <=1;
+           o_alm_full <=0;
+          o_empty <= 0;// Update empty signal
+        o_full <= 0;
         end
       
-//       else
-//         begin
-//            o_full <= 0; // Update full signal
-//          o_empty <= 0;
-//         end
+      else
+        begin
+           o_full <= 0; // Update full signal
+         o_empty <= 0;
+          o_alm_full <= 0;
+          o_alm_empty <=0;
+        end
     end
   end
 
 endmodule
+
+
+
+
