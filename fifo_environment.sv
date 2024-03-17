2356eb61135735accb2ebebfbda0bd7969b6aa1d
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2024-03-17 10:43:04 +0530

    Update fifo_environment.sv

--- fifo_environment.sv
+++ fifo_environment.sv
@@ -1,6 +1,8 @@
 class fifo_environment extends uvm_env;
   fifo_agent agt;
+  fifo_sub coverage_h;
   fifo_scoreboard scb;
+ 
   `uvm_component_utils(fifo_environment)
   
   function new(string name = "fifo_environment", uvm_component parent);
@@ -10,10 +12,15 @@ class fifo_environment extends uvm_env;
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     agt = fifo_agent::type_id::create("agt", this);
+    coverage_h = fifo_sub::type_id::create("coverage_h", this);
     scb = fifo_scoreboard::type_id::create("scb", this);
+    
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    //monitor to coverage
+    agt.mon.item_got_port.connect(coverage_h.item_got_export1);
     //monitor to scoreboard connection
     agt.mon.item_got_port.connect(scb.item_got_export);
     uvm_report_info("FIFO_ENVIRONMENT", "connect_phase, Connected monitor to scoreboard");
